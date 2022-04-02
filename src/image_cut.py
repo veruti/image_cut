67079988ebc1d0cfd17e7f5734e530f3b76b2a1d
mlepiavko <mlepiavko@issart.com>
2022-04-02 12:12:51 +0700

    add image cutter

---
+++ src/image_cut.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_start_finish_coords(image_size, tile_size, tile_shift):
+    start = np.arange(0, image_size, tile_size)
+    start[1:] = start[1:] - tile_shift
+
+    if image_size <= (start[-1] + tile_size):
+        start = start[:-1]
+
+    finish = start + tile_size
+    if finish[-1] != (image_size - 1):
+        start = np.append(start, image_size - tile_size - 1)
+        finish = np.append(finish, image_size - 1)
+
+    return list(zip(start, finish))
+
+
+def cut_image(
+        image: np.array,
+        x_width: int,
+        y_height: int,
+        x_shift: int,
+        y_shift: int
+):
+    image_height, image_width = image.shape[:2]
+
+    x_pairs = get_start_finish_coords(image_width, x_width, x_shift)
+    y_pairs = get_start_finish_coords(image_height, y_height, y_shift)
+
+    tiles = []
+    for x in x_pairs:
+        x_s, x_f = x
+        for y in y_pairs:
+            y_s, y_f = y
+
+            tiles.append(image[y_s:y_f, x_s:x_f])
+            print(tiles[-1].shape)
+            plt.imshow(tiles[-1])
+            plt.show()
